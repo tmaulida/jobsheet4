fd662274814cc90ce767ab2bf7d64d0152d313e2
tmaulida <thoyibatulmaulidaaa@gmail.com>
2025-03-10 12:36:32 +0700

    Pesan Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                     <h1>Profil Saya</h1>
+               </div>
+               <div class="card-body">
+                    <img src="profile.jpeg" alt="Profile Picture" width="150">
+                    <h3>Thoyibatulmaulidaaa</h3>
+                    <p>Nama: Thoyibatul Maulida</p>
+                    <p>Email: thoyibatulmaulidaaa@gmail.com</p>
+                    <p>Bio: Siswa SMKN 1 sayung</p>
+                    <h5> Skills: </h5>
+                    <ul>
+                        <li>Manajemen Waktu</li>
+                        <li>Komunikasi</li>
+                        <li>Desain Grafis</li>
+                   </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
